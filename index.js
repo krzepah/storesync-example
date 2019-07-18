e3fa44f432df09e50c45e6b73108e60f6a2755a3
Patrick Borowy <patrick.borowy@gmail.com>
2019-07-18 21:31:34 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,108 @@
+import { h, Component } from 'preact';
+import { Router } from 'preact-router';
+import createStore from 'mutastore'
+import { Provider, connect } from 'unistore/preact'
+
+import { map } from 'ramda';
+import { merge, filter, concat } from 'ramda';
+
+import sync from 'mutasync'
+
+import { mutations, defaults } from './mutations';
+
+let store = createStore(
+  defaults,
+  mutations
+)
+
+const SYNC = sync({
+  host: 'http://localhost:2017',
+  app: '15ryxhs6rer634l73wvhh8'
+})
+
+store.subscribe(SYNC.apply)
+
+store.subscribe((
+  state, update, overwrite, act
+) => console.log('->', state, act))
+
+const LoginBase = ({login}) => <form onSubmit={
+  (e) => {
+    e.preventDefault();
+    login( e.target[0].value, e.target[1].value )
+  }
+}>
+  <label>Username</label>
+  <input type="text" id="email" />
+  <label>Password</label>
+  <input type="password" id="password" />
+  <button type="submit">Login</button>
+  <button onClick={(e) => {
+    e.preventDefault();
+    sync('mytoken').clearLocal(defaults)
+    store.mutate({logout: defaults}, true)
+  }}>Logout</button>
+</form>
+
+const Login = connect(
+  (state, props) => ({...state}),
+  (state, props) => ({
+    login: (state, username, password) => SYNC
+      .auth( username, password )
+      .then( (res) => res.json() )
+      .then( (res) => ({ authenticate: { ...res }, sync: false }))
+      .catch(function(res) {
+         console.log('error :', res); 
+      })
+  })
+)(LoginBase)
+
+const PageBase = ({elements, elementsIds, add}) => <div
+  style="padding: 10px;"
+>
+  <Login />
+  <br />
+  <input id="input" />
+  <button onClick={() => {
+    add(document.getElementById('input').value,
+      Math.random().toString(36).substring(7)
+    );
+    document.getElementById('input').value = "";
+  }}>
+    add
+  </button>
+  <br />
+  elements: 
+  { map(
+      (id) => <div style="padding: 20px; border-bottom: solid 1px;">
+        {elements[id]}
+      </div>,
+      elementsIds
+  )}
+</div>
+
+const Page = connect(
+  (state, props) => ({...state}),
+  (state, props) => ({
+    add: (state, text, newId) => ({
+      newElement: {
+        text: text,
+        newId: newId
+      }
+    })
+  })
+)(PageBase)
+
+class App extends Component {
+  componentDidMount = () => store.setState(
+    sync('mytoken').retrieveLocal()
+  )
+
+  render = () => <div id="app">
+    <Provider store={store}>
+      <Page />
+    </Provider>
+  </div>
+}
+
+export default App;
